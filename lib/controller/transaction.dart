f4b38244c514d4ed3d8dfa8b349dc11bb618f125
joshua <mama27j@gmail.com>
2020-07-01 21:21:06 +0300

    add transaction controller

--- lib/controller/transaction.dart
+++ lib/controller/transaction.dart
@@ -1,47 +1,117 @@
+import 'package:bfast/adapter/rest.dart';
 import 'package:bfast/adapter/transaction.dart';
+import 'package:bfast/controller/rest.dart';
 import 'package:bfast/model/transaction.dart';
 
-class TransactionController implements TransactionAdapter{
+import '../configuration.dart';
+
+class TransactionController implements TransactionAdapter {
+  List<TransactionModel> transactionRequests;
+  bool _isNormalBatch;
+  RestAdapter _restAdapter;
+  String _appName;
+
+  TransactionController(String appName, RestAdapter restAdapter,
+      {bool isNormalBatch = false}) {
+    this._isNormalBatch = isNormalBatch;
+    this._appName = appName;
+    this._restAdapter = restAdapter;
+  }
+
   @override
-  Future commit({Future<List<TransactionModel>> Function(List<TransactionModel> transactionsRequests) before, Future<void> Function() after, bool useMasterKey}) {
-    // TODO: implement commit
-    throw UnimplementedError();
+  Future commit(
+      {Future<List<TransactionModel>> Function(
+              List<TransactionModel> transactionsRequests)
+          before,
+      Future<void> Function() after,
+      bool useMasterKey}) async {
+    if (before != null) {
+      List<TransactionModel> result = await before(this.transactionRequests);
+      if (result != null &&
+          result is List &&
+          result.length > 0 &&
+          result[0].body != null &&
+          result[0].path != null &&
+          result[0].method != null) {
+        this.transactionRequests = result;
+      } else if (result != null && result is List && result.length == 0) {
+        this.transactionRequests = result;
+      }
+    }
+    RestResponse response = await this._restAdapter.post(
+        '${BFastConfig.getInstance().databaseURL(this._appName, '/batch')}',
+        {
+          "requests": this.transactionRequests,
+          "transaction": !this._isNormalBatch,
+        },
+        RestRequestConfig(
+          headers: (useMasterKey == true)
+              ? BFastConfig.getInstance().getMasterHeaders(this._appName)
+              : BFastConfig.getInstance().getHeaders(this._appName),
+        ));
+    this.transactionRequests = [];
+    if (after != null) {
+      await after();
+    }
+    return response.data;
   }
 
   @override
   TransactionAdapter create(String domainName, Map data) {
-    // TODO: implement create
-    throw UnimplementedError();
+    this.transactionRequests.add(TransactionModel(
+        body: data, method: "POST", path: '/classes/$domainName'));
+    return this;
   }
 
   @override
   TransactionAdapter createMany(String domainName, List<Map> data) {
-    // TODO: implement createMany
-    throw UnimplementedError();
+    List<TransactionModel> trans = data.map<TransactionModel>((payLoad) {
+      return TransactionModel(
+          body: payLoad, method: "POST", path: '/classes/$domainName');
+    }).toList();
+    this.transactionRequests.addAll(trans);
+    return this;
   }
 
   @override
   TransactionAdapter delete(String domainName, DeletePayload payload) {
-    // TODO: implement delete
-    throw UnimplementedError();
+    this.transactionRequests.add(TransactionModel(
+        body: payload?.data != null ? payload.data : {},
+        method: "DELETE",
+        path: '/classes/$domainName/${payload.objectId}'));
+    return this;
   }
 
   @override
   TransactionAdapter deleteMany(String domainName, List<DeletePayload> data) {
-    // TODO: implement deleteMany
-    throw UnimplementedError();
+    List<TransactionModel> trans = data.map<TransactionModel>((payLoad) {
+      return TransactionModel(
+          body: payLoad?.data != null ? payLoad.data : {},
+          method: "DELETE",
+          path: '/classes/$domainName/${payLoad.objectId}');
+    });
+    this.transactionRequests.addAll(trans);
+    return this;
   }
 
   @override
   TransactionAdapter update(String domainName, UpdatePayLoad payLoad) {
-    // TODO: implement update
-    throw UnimplementedError();
+    this.transactionRequests.add(TransactionModel(
+        body: payLoad.data,
+        method: "PUT",
+        path: '/classes/$domainName/${payLoad.objectId}'));
+    return this;
   }
 
   @override
   TransactionAdapter updateMany(String domainName, List<UpdatePayLoad> data) {
-    // TODO: implement updateMany
-    throw UnimplementedError();
+    List<TransactionModel> trans = data.map<TransactionModel>((payLoad) {
+      return TransactionModel(
+          body: payLoad.data,
+          method: "PUT",
+          path: '/classes/$domainName/${payLoad.objectId}');
+    });
+    this.transactionRequests.addAll(trans);
+    return this;
   }
-
 }
--- lib/model/transaction.dart
+++ lib/model/transaction.dart
@@ -3,7 +3,7 @@ class TransactionModel {
   String path;
   Map<String, dynamic> body;
 
-  TransactionModel(String path, String method, Map<String, dynamic> body) {
+  TransactionModel({String path, String method, Map<String, dynamic> body}) {
     this.method = method;
     this.path = path;
     this.body = body;
